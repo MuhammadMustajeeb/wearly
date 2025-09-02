637b59bd6a90e41ef97532b8f16b246338df1e05
Muhammad Mustajeeb <mprogramming48@gmail.com>
2025-09-02 05:33:16 -0700

    first commit

---
+++ models/User.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+// creating a schema for the user
+const userSchema = new mongoose.Schema({
+    _id: {type: String, required: true},
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    imageURl: {type: String, required: true},
+    cartItems: {type: Object, default: {}},
+}, { minimize: false });
+
+const User = mongoose.models.user || mongoose.model('  user', userSchema); // this the user model based on the schema created above
+
+export default User;
